a87d4e3ece34a20089fa8564686dc7e0dcc2d93b
Lillfilly <Lillfilly@hotmail.com>
2013-02-22 21:25:46 +0100

    ...

--- filmhandler/src/se/ixanon/filmhandler/client/Main.java
+++ filmhandler/src/se/ixanon/filmhandler/client/Main.java
@@ -1,17 +1,32 @@
 package se.ixanon.filmhandler.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TabPanel;
 
 public class Main implements EntryPoint {
 
+	TabPanel tabPanel = new TabPanel();
+	String[] tabTitles = new String[] {"Channels", "Videos"};
+	
 	@Override
 	public void onModuleLoad() {
 		
-		//Add the Filehandlerthing   including upload.
+//		setupTabPanel();
+//		RootPanel.get("TabTest").add(tabPanel);
+		
+		//Add the Filehandlerthing
 		@SuppressWarnings("unused")
 		FileList fileList = new FileList();
+		RootPanel.get("Filmhanteraren").add(fileList.getWidget());
+	}
+	private void setupTabPanel()
+	{
+		tabPanel.add(new HTML("Channel Configuration"), tabTitles[0]);
+		tabPanel.add(new HTML("<h3>Video Config</h3>"), tabTitles[1]);
 		
-		//TODO add feature: Channel-settings   XML
+		tabPanel.selectTab(0);
 	}
 
 }
