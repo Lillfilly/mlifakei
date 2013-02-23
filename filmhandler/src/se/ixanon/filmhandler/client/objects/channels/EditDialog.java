18cca17f0287071518c3ad3ec5f76d6ca1330d47
Lillfilly <Lillfilly@hotmail.com>
2013-02-23 20:59:23 +0100

    ...

--- filmhandler/src/se/ixanon/filmhandler/client/objects/channels/EditDialog.java
+++ filmhandler/src/se/ixanon/filmhandler/client/objects/channels/EditDialog.java
@@ -1,5 +1,31 @@
 package se.ixanon.filmhandler.client.objects.channels;
 
-public class EditDialog {
+import se.ixanon.filmhandler.client.services.ChannelService;
+import se.ixanon.filmhandler.client.services.ChannelServiceAsync;
+import se.ixanon.filmhandler.shared.Channel;
 
+import com.google.gwt.core.shared.GWT;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public class EditDialog extends DialogBox {
+	
+	ChannelServiceAsync channelService = GWT.create(ChannelService.class);
+	
+	VerticalPanel vPanel = new VerticalPanel();
+	
+	HTML header = new HTML();
+	ListBox videoList = new ListBox(false);
+	
+	public EditDialog(Channel channelToBeEdited) {
+		
+		header.setHTML("<h3>Configure Channel: " + channelToBeEdited.name + "</h3>");
+		
+		videoList.ensureDebugId("cwListBox-dropBox");
+		
+		vPanel.add(header);
+		vPanel.add(videoList);
+	}
 }
