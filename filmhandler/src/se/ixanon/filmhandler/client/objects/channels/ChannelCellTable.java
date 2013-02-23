3879a5f406a87c9611ef84c99a74a0ceffdcbd60
chriph <coffeechriph@gmail.com>
2013-02-23 21:22:38 +0100

    ...

--- filmhandler/src/se/ixanon/filmhandler/client/objects/channels/ChannelCellTable.java
+++ filmhandler/src/se/ixanon/filmhandler/client/objects/channels/ChannelCellTable.java
@@ -60,8 +60,7 @@ public class ChannelCellTable extends CellTable<Channel> {
 	public ChannelCellTable() {
 		editCol.setFieldUpdater(new FieldUpdater<Channel, String>() {	
 			@Override
-			public void update(int index, Channel object, String value) {
-				Window.alert("This is a secret message that only will appear if Oliver doesnt fail when trying to get the editbutton working!");
+			public void update(int index, Channel object, String value) {			
 				EditDialog editDialog = new EditDialog(object, index);
 				editDialog.center();
 			}
